2a5a64e5a89e28d8adf08575f076f785777361ed
nukero <solomider12@gmail.com>
2020-12-11 12:20:33 +0200

    changed setting

--- crm1/settings.py
+++ crm1/settings.py
@@ -165,8 +165,6 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals())
-
 
 LOGGING = {
     'version': 1,
