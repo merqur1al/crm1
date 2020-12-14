34ea3d05b353af8aaa6c6a49d68bdd357bf76346
nukero <solomider12@gmail.com>
2020-12-14 14:48:52 +0200

    changed setting

--- crm1/settings.py
+++ crm1/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 
 
 #SMTP Cnfiguration
